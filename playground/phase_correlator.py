98a63776cd5e91a6349e5cf0e95b4a882f702328
dziyaee <dziyaee@gmail.com>
2020-06-16 12:55:55 -0700

    standardized variables and methods used during initial phase correlation and plot update phase correlations. in process of adding functions to reduce repetition throughout script

--- playground/phase_correlator.py
+++ playground/phase_correlator.py
@@ -1,10 +1,8 @@
 import numpy as np
-from numpy import log10
 from scipy.fftpack import fft2
 import matplotlib as mpl
-from matplotlib.widgets import Cursor
 import matplotlib.pyplot as plt
-from dzlib.common.utils import info, stats
+from matplotlib.widgets import Cursor
 mpl.use("Qt5Agg")
 
 
@@ -57,22 +55,33 @@ def add_gnoise2d(x, *args):
     return x, snr
 
 
-# amplitude, span, and initial coords of non-zero values in image
+def image_init(A, h, w, N1, N2, n1, n2, noise_mean, noise_std):
+    x = np.zeros((N1, N2))
+    x[n1: n1+h, n2: n2+w] = A
+    x, _ = add_gnoise2d(x, noise_mean, noise_std)
+    return x
+
+def image_processing(x1, x2):
+    X1, X2 = ff2(x1), ff2(x2)
+    x3 = phase_corr(X1, X2)
+    x3 = np.abs(x3)
+    e1, e2 = np.where(x3 == np.max(x3))
+    return x3, (e1, e2)
+
+
+# Image Initialization
+## amplitude, span, and initial coords of non-zero values in image
 A = 5
 h, w = 5, 5
-global x1n1, x1n2, x2n1, x2n2
-x1n1, x1n2 = 0, 0
-x2n1, x2n2 = 0, 0
 
-# full image span
-global N1, N2
+## span of full image
 N1, N2 = 50, 50
 
-# noise characteristics
+## noise characteristics
 noise_mean = 0
 noise_std = 0.4
 
-# init images with zeros, non-zeros, then add noise
+## init images with zeros, non-zeros, then add noise
 x1 = np.zeros((N1, N2))
 x1[0: 0+h, 0: 0+w] = A
 x1, _ = add_gnoise2d(x1, noise_mean, noise_std)
@@ -81,106 +90,92 @@ x2 = np.zeros((N1, N2))
 x2[0: 0+h, 0: 0+w] = A
 x2, _ = add_gnoise2d(x2, noise_mean, noise_std)
 
-# DFT of images
+# Image Processing
+## DFT of images
 X1, X2 = fft2(x1), fft2(x2)
 
-# phase correlation, time-domain impulse peaks
+## phase correlation, time-domain impulse peaks
 x3 = phase_corr(X1, X2)
 x3 = np.abs(x3)
 e1, e2 = np.where(x3 == np.max(x3))
 
-# Initial Plot
+# Initial Plots
 fig, axes = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
 fig.subplots_adjust(wspace=0)
-[ax1, ax2, ax3] = axes
+[ax0, ax1, ax2] = axes
 
+## to keep contrast between both images consistent
 vmin = min(np.min(x1), np.min(x2))
 vmax = max(np.max(x1), np.max(x2))
 
-img1 = ax1.imshow(x1, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin=vmin, vmax=vmax)
-img2 = ax2.imshow(x2, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin=vmin, vmax=vmax)
-img3 = ax3.imshow(x3, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='Reds')
+## extent to properly center imshow pixels
+img1 = ax0.imshow(x1, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin=vmin, vmax=vmax)
+img2 = ax1.imshow(x2, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin=vmin, vmax=vmax)
+img3 = ax2.imshow(x3, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='Reds')
 
+## titles, ticks, labels
 fig.suptitle("Motion Estimation via Phase Correlation")
-ax1.set_title("Original Image", fontsize=10)
-ax2.set_title(f"Shifted Image\n Actual Shift: {x2n1-x1n1, x2n2-x1n2}", fontsize=10)
-ax3.set_title(f"Motion Estimate\n Estimated Shift: {e1[0], e2[0]}", fontsize=10)
+ax0.set_title("Original Image", fontsize=10)
+ax1.set_title(f"Shifted Image\n Actual Shift: {0, 0}", fontsize=10)
+ax2.set_title(f"Motion Estimate\n Estimated Shift: {e1[0], e2[0]}", fontsize=10)
+ax1.tick_params(left=False)
+ax2.tick_params(left=False, right=True, labelright=True)
 
-ax2.tick_params(left=False)
-ax3.tick_params(left=False, right=True, labelright=True)
+# Collect data for easier indexing
+data1 = {'X': X1, 'pos': (0, 0), 'img': img1}
+data2 = {'X': X2, 'pos': (0, 0), 'img': img2}
+data3 = {'X': None, 'pos': (0, 0), 'img': img3}
+datas = {ax0: data1, ax1: data2, ax2: data3}
 
-# Data for plot updates
-axes_dict = {ax1: 0, ax2: 1}
-X_list = [X1, X2]
-img_list = [img1, img2]
-
-# Plot updates
-global clicking
+# Plot update functions
 clicking = False
 
+## true if LMB click
 def mouse_click(event):
     global clicking
-    global ax
-    if event.inaxes is None:
-        return
-    if event.button != 1:
-        return
-    clicking = True
-    # ax = event.inaxes
+    if event.button == 1:
+        clicking = True
 
-
+## false if LMB release
 def mouse_release(event):
     global clicking
-    if event.inaxes is None:
-        return
-    if event.button != 1:
-        return
-    clicking = False
+    if event.button == 1:
+        clicking = False
 
-
+## if clicking and in first two axes, update images, motion estimation, then plot
 def mouse_move(event):
-    global x1n1, x1n2
-    global x2n1, x2n2
-
     x, y = event.xdata, event.ydata
+    ax = event.inaxes
 
-    if (clicking and event.inaxes in axes_dict):
-
-        # get axis index
-        i = axes_dict[event.inaxes]
+    if (clicking and ax in axes[:2]):
 
         # get new image coords
         n1, n2 = int(y), int(x)
+        datas[ax]['pos'] = (n1, n2)
 
-        if i == 0:
-            x1n1, x1n2 = n1, n2
-        elif i ==1:
-            x2n1, x2n2 = n1, n2
-
-        # create image and compute DFT
+        # Image Initialization
+        ## create new noisy image and compute DFT
         x = np.zeros((N1, N2))
         x[n1: n1+h, n2: n2+w] = A
+        x, snr = add_gnoise2d(x, noise_mean, noise_std)
 
-        x, _ = add_gnoise2d(x, noise_mean, noise_std)
+        # Image Processing
+        ## DFT of image
         X = fft2(x)
+        datas[ax]['X'] = X
 
-        # update DFT list and compute phase correlation time-domain impulse
-        X_list[i] = X
-        x3 = phase_corr(*X_list)
+        ## phase correlation, time-domain impulse peaks
+        x3 = phase_corr(datas[ax0]['X'], datas[ax1]['X'])
         x3 = np.abs(x3)
         e1, e2 = np.where(x3 == np.max(x3))
+        datas[ax2]['pos'] = (e1[0], e2[0])
 
-        # update image plots
-        img_list[i].set_data(x)
-        img3.set_data(x3)
-
-        # Other plot stuff
-        ax1.set_title("Original Image", fontsize=10)
-        ax2.set_title(f"Shifted Image\n Actual Shift: {x2n1-x1n1, x2n2-x1n2}", fontsize=10)
-        ax3.set_title(f"Motion Estimate\n Estimated Shift: {e1[0], e2[0]}", fontsize=10)
-
-        # update axis info
-        # ax3.set_title(f"k1 = {k1[0]}, k2 = {k2[0]}")
+        # update axes
+        datas[ax]['img'].set_data(x)
+        datas[ax2]['img'].set_data(x3)
+        ax0.set_title("Original Image", fontsize=10)
+        ax1.set_title(f"Shifted Image\n Actual Shift: {tuple(np.subtract(datas[ax1]['pos'], datas[ax0]['pos']))}", fontsize=10)
+        ax2.set_title(f"Motion Estimate\n Estimated Shift: {datas[ax2]['pos']}", fontsize=10)
         fig.canvas.draw_idle()
 
 
