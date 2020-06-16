06298a200ed0ab7af1e449272c7e42e28d37d770
dziyaee <dziyaee@gmail.com>
2020-06-16 11:26:59 -0700

    added phase correlation summary comments, in process of cleaning up variables

--- playground/phase_correlator.py
+++ playground/phase_correlator.py
@@ -10,20 +10,43 @@ mpl.use("Qt5Agg")
 
 eps = 1e-6
 def phase_corr(X1, X2):
-    # Calculate the cross-power spectrum, which is given by the Hadamard (or element-wise) product of x1 and the complex conjugate of x2
-    # Note: An epsilon value is used here to deal with divide-by-zero
+    """
+    Function to calculate the phase correlation between two 2d signals
+    The power spectrum of a signal can be calculated by multiplying the signal DFT by it's own complex conjugate. In doing so, the phases of each component cancel out, while the magnitudes multiply together to give the power.
+    As a matrix operation, the above multiplication is an element-wise multiplication, otherwise known as the Hadamard Product.
+    In order to obtain the phase shift resulting from a time shift of a signal, the cross-power spectrum (cps) is used. In this case, there are two signals, the original signal DFT (X1), and the time-shifted signal DFT (X2). The cross-power spectrum is given by the Hadamard Product of the original signal DFT and the complex conjugate of the time-shifted signal DFT. The resulting phase component of the output is due to the time shift of the signal, and the magnitudes once again multiply together to give the power.
+        Note: An epsilon value (eps) is used here to deal with divide-by-zero errors. The magnitude of the eps may need to be adjusted to keep it very small in comparison to the signal magnitudes
+
+    Args:
+        X1 (TYPE): Description
+        X2 (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
+    N1, N2 = X1.shape
+
+    # cross-power spectrum
     cps = np.multiply(X1, np.conj(X2)) + eps
 
-    # Normalize the cross-power spectrum by dividing it with it's own magnitude
+    # normalized cross-power spectrum
     ncps = cps / np.abs(cps)
 
-    # Calculate the normalized cross-correlation by computing the Inverse DFT
+    # normalized cross-correlation
     ncc = (1 / N1 * N2) * fft2(ncps)
-    x3 = np.abs(ncc)
-    return x3
+    return ncc
 
 
 def add_gnoise2d(x, *args):
+    """Summary
+
+    Args:
+        x (TYPE): Description
+        *args: Description
+
+    Returns:
+        TYPE: Description
+    """
     N1, N2 = x.shape
     mean, std = 0, 1
     if args:
@@ -34,37 +57,36 @@ def add_gnoise2d(x, *args):
     return x, snr
 
 
-# Image information
-# A: non-zero amplitude
-# N1, N2: number of rows, cols
-# n1, n2 non-zero starting indices for row, col
-# h, w: non-zero height, width
-# k1, k2: time-shift values for row, col
+# amplitude, span, and initial coords of non-zero values in image
 A = 5
-N1, N2 = 50, 50
-n1, n2 = 0, 0
-x1n1, x1n2 = n1, n2
 h, w = 5, 5
-k1, k2 = 0, 0
-x2n1, x2n2 = n1 + k1, n2 + k2
-zeros = np.zeros((N1, N2))
+global x1n1, x1n2, x2n1, x2n2
+x1n1, x1n2 = 0, 0
+x2n1, x2n2 = 0, 0
+
+# full image span
+global N1, N2
+N1, N2 = 50, 50
+
+# noise characteristics
 noise_mean = 0
 noise_std = 0.4
 
-# Image init; starting image, x1; translated image, x2
+# init images with zeros, non-zeros, then add noise
 x1 = np.zeros((N1, N2))
-x1[n1: n1+h, n2: n2+w] = A
+x1[0: 0+h, 0: 0+w] = A
 x1, _ = add_gnoise2d(x1, noise_mean, noise_std)
 
 x2 = np.zeros((N1, N2))
-x2[n1+k1: n1+k1+h, n2+k2: n2+k2+w] = A
+x2[0: 0+h, 0: 0+w] = A
 x2, _ = add_gnoise2d(x2, noise_mean, noise_std)
 
-# DFT of Initial Image x1, X1; DFT of Translated Image x2, X2
+# DFT of images
 X1, X2 = fft2(x1), fft2(x2)
 
-# Phase Correlation Time-Domain impulse
+# phase correlation, time-domain impulse peaks
 x3 = phase_corr(X1, X2)
+x3 = np.abs(x3)
 e1, e2 = np.where(x3 == np.max(x3))
 
 # Initial Plot
@@ -75,12 +97,13 @@ fig.subplots_adjust(wspace=0)
 vmin = min(np.min(x1), np.min(x2))
 vmax = max(np.max(x1), np.max(x2))
 
-img1 = ax1.imshow(x1, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin = vmin, vmax = vmax)
-img2 = ax2.imshow(x2, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin = vmin, vmax = vmax)
+img1 = ax1.imshow(x1, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin=vmin, vmax=vmax)
+img2 = ax2.imshow(x2, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='binary', vmin=vmin, vmax=vmax)
 img3 = ax3.imshow(x3, extent=[0, N2, 0, N1], origin='lower', interpolation='None', cmap='Reds')
 
+fig.suptitle("Motion Estimation via Phase Correlation")
 ax1.set_title("Original Image", fontsize=10)
-ax2.set_title(f"Shifted Image\n Actual Shift: {k1, k2}", fontsize=10)
+ax2.set_title(f"Shifted Image\n Actual Shift: {x2n1-x1n1, x2n2-x1n2}", fontsize=10)
 ax3.set_title(f"Motion Estimate\n Estimated Shift: {e1[0], e2[0]}", fontsize=10)
 
 ax2.tick_params(left=False)
@@ -94,8 +117,6 @@ img_list = [img1, img2]
 # Plot updates
 global clicking
 clicking = False
-global ax
-ax = None
 
 def mouse_click(event):
     global clicking
@@ -137,7 +158,7 @@ def mouse_move(event):
             x2n1, x2n2 = n1, n2
 
         # create image and compute DFT
-        x = zeros.copy()
+        x = np.zeros((N1, N2))
         x[n1: n1+h, n2: n2+w] = A
 
         x, _ = add_gnoise2d(x, noise_mean, noise_std)
@@ -146,6 +167,7 @@ def mouse_move(event):
         # update DFT list and compute phase correlation time-domain impulse
         X_list[i] = X
         x3 = phase_corr(*X_list)
+        x3 = np.abs(x3)
         e1, e2 = np.where(x3 == np.max(x3))
 
         # update image plots
