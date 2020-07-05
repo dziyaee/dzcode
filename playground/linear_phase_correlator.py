5ac12b77e31cb338c01c8448f47e347345dd1d24
dziyaee <dziyaee@gmail.com>
2020-07-05 15:49:33 -0700

    Implemented Signal class for tracking of signal information (such as magnitude, phase, stats). Added SignalCoords (subclass of Signal) to additionally track xy-coordinates associated with a signal within a scene. Implemented CyclicData class to track and plot signal statistics. Cleaned up and standardized the implementation of the phase correlation, peak detection, and motion estimation algorithms

--- playground/linear_phase_correlator.py
+++ playground/linear_phase_correlator.py
@@ -1,163 +1,218 @@
+# Imports
+# general
+import os
+from PIL import Image
 import numpy as np
-from numpy import pi, exp, cos, sin
+
+# scipy
+from scipy.fft import fft2, fftshift
+from scipy.signal import windows
+
+# dzlib
+# from dzlib.signal_processing.signals import signal
+from dzlib.signal_processing.signals import Signal
+from dzlib.signal_processing.utils import phase_correlation2d, window2d
+from dzlib.common.utils import Cyclic_Data
+
+# matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 import matplotlib.patches as patches
-from scipy.fft import fft2, ifft2, fftshift
-from scipy.signal import windows
-from dzlib.signal_processing.signals import signal, printsignal, printstats
-from PIL import Image
-import os
 
 
-def phase_corr2d(X1, X2):
-    X3 = np.multiply(X1, np.conj(X2))
-    X3 = X3 / np.abs(X3)
-    x3 = ifft2(X3)
-    x3 = fftshift(x3)
-    return x3
+class SignalCoords(Signal):
+    def __init__(self, array, xy):
+        super().__init__(array)
+        self.x, self.y = xy
+
+    def set_xy(self, xy):
+        self.x, self.y = xy
 
 
 def coords_bottomleft(coords_center, scene_shape, image_shape):
-    """Transforms (x, y) center coords to bottom left coords of an image within the span of a scene
-    """
+    """Transforms the center coords of an image within a scene to the bottom-left coords"""
     x, y = coords_center
-    N1, N2 = scene_shape
-    w, h = image_shape
+    N2, N1 = scene_shape
+    h, w = image_shape
     x = int(min(max(x, w/2), N1-w/2) - w/2)
     y = int(min(max(y, h/2), N2-h/2) - h/2)
     return (x, y)
 
 
-def update_ydatas(x):
-    return x.max, x.var
+def new_ydata(x):
+    """Function to specify the data to track"""
+    return np.var(x.real), np.var(x.mag)
 
 
-# Starting Params
-global eps, i, n_points
+def unroll_shift(x, N):
+    """Function to transform shift coords span from (-N/2 to +N/2) to (0 to N)"""
+    z = x%(N/2) - (x//(N/2))*(N/2)
+    return int(z)
+
+
+# starting params
+global eps
 eps = 1e-6
-n_points = 200
-i = 0
-N2, N1 = 400, 400
-h, w = 100, 100
-n2, n1 = 100, 100
-m2, m1 = 100, 100
+N2, N1 = 400, 400 # scene span (rows, cols) or (height, width)
+h, w = 100, 100 # image span
+n2, n1 = 100, 100 # image1 bottom-left coords
+m2, m1 = 100, 100 # image2 bottom-left coords
 
-# init scene, window, and images
+# Some arrays are stored in my custom 'Signal' namedtuple with the following structure:
+# Signal = namedtuple('Signal', 'arr real imag mag phase min max mean std var')
+# arr stores the array itself, min, max, mean, std, var are all calculated on the array's magnitude
+
+# Naming convention for arrays: lowercase for non-frequency domain, uppercase for frequency-domain. Example: x1 is a time-domain signal. DFT of x1 = X1, a frequency-domain signal. IDFT of X1 = x1
+
+# init scene
 scene_path = '/'.join((os.getcwd(), 'data/image2.png'))
 scene = Image.open(scene_path).convert("L")
 scene = np.asarray(scene.resize((N2, N1))).astype(np.float32)
-scene = np.flip(scene, axis=(0))
-scene = signal(scene)
+scene = np.flip(scene, axis=(0)) # flip to account for imshow(center='lower')
+scene = Signal(scene) # stored as Signal class
+
+# init 2d window
 window = windows.triang(w)
-window = np.outer(window, window)
+window = window2d(window) # create a 2d window out of a 1d window
+
+# init images
 x1 = scene.arr[n2: n2+h, n1: n1+w]
 x1 = np.multiply(window, x1)
-x1 = signal(x1)
+x1 = SignalCoords(x1, (n1, n2))
 x2 = scene.arr[m2: m2+h, m1: m1+w]
 x2 = np.multiply(window, x2)
-x2 = signal(x2)
+x2 = SignalCoords(x2, (m1, m2))
 
-# Image processing
+# image processing
 X1, X2 = fft2(x1.arr), fft2(x2.arr)
-x3 = phase_corr2d(X1, X2)
-x3 = signal(x3)
+x3 = phase_correlation2d(X1, X2) # returns the normalized cross-correlation between X1 and X2
+x3 = fftshift(x3)
+x3 = Signal(x3)
 
 # calc shifts
-a1, a2 = n1-n1, n2-n2 # actual shift
-e2, e1 = np.where(x3.mag == np.max(x3.mag)) # estimated shift
-e1, e2 = e1[0] - int(w/2), e2[0] - int(h/2) # adjusted for center origin plot
-peak = np.where(x3.mag.flatten() == x3.max)[0]
+dx = x2.x - x1.x # actual shift
+dy = x2.y - x1.y
+edy, edx = [int(x) for x in np.where(x3.real == x3.max)] # estimated shift
+edx -= int(w/2) # adjust for fftshift of x3
+edy -= int(h/2)
+edx = unroll_shift(edx, w)
+edy = unroll_shift(edy, h)
+peakreal = np.where(x3.real.flatten() == x3.max)[0][0]
+peakmag = np.where(x3.mag.flatten() == np.max(x3.mag))[0][0]
 
 # Init data tracking
-xdata = np.arange(n_points)
-ydatas = np.zeros((n_points, 2))
-ydatas[i, :] = update_ydatas(x3)
+n_xdata = 200
+xdata = np.arange(n_xdata)
+n_ydata = len(new_ydata(x3))
+ydatas = Cyclic_Data(n_xdata, n_ydata) # cyclic data array class
+ydatas.update(new_ydata(x3)) # new values will replace old values after reaching the end
+i = ydatas.i
 
-# Set up figure1 and subplots
+# Set up figure and subplots
 fig1 = plt.figure(figsize=(14, 5))
-gs = GridSpec(nrows=3, ncols=15)
+gs = GridSpec(nrows=4, ncols=15)
 ax0 = fig1.add_subplot(gs[0:2, 0:3])
 ax1 = fig1.add_subplot(gs[0:2, 4:8])
 ax2 = fig1.add_subplot(gs[0:2, 9:12])
 ax3 = fig1.add_subplot(gs[0:2, 12:15])
-ax4 = fig1.add_subplot(gs[2:3, 0:3])
-ax5 = fig1.add_subplot(gs[2:3, 4:7])
-ax6 = fig1.add_subplot(gs[2:3, 8:15])
-axes_focus = [ax2, ax3]
+ax4 = fig1.add_subplot(gs[2:3, 0:7])
+ax5 = fig1.add_subplot(gs[2:3, 8:15])
+ax6 = fig1.add_subplot(gs[3:4, 0:7])
+ax7 = fig1.add_subplot(gs[3:4, 8:15])
 axes_stats = [ax4, ax5]
 fig1.subplots_adjust(left=0.04, right=0.99, hspace=0.5, wspace=0)
+fig1.tight_layout()
 
-for ax in axes_focus:
+# ticks, lims, titles
+for ax in [ax2, ax3]:
     ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
-for ax, ydata in zip(axes_stats, ydatas.T):
-    ax.set_xlim(0, n_points)
+for ax, ydata in zip(axes_stats, ydatas.data.T):
+    ax.set_xlim(0, n_xdata)
     ax.set_ylim(0, ydata[i]*1.1)
-
-ax0.set_title(f'Scene\nActual Shift = {a1, a2}')
-ax1.set_title('$x_{3mag}$ \n'f'Estimated Shift = {e1, e2}')
+ax0.set_title(f'Scene\nActual Shift = {dx, dy}')
+ax1.set_title('$x_{3real}$ \n'f'Estimated Shift = {edx, edy}')
 ax2.set_title(f'Image 1 Focus')
 ax3.set_title(f'Image 2 Focus')
-ax4.set_title('$MAX(x_{3mag})$')
-ax5.set_title('$VAR(x_{3mag})$')
-ax6.set_title('$Flattened x_{3mag}$ \n'f'Peak at: {peak}')
+ax4.set_title('Variance $(x_{3real})$')
+ax5.set_title('Variance $(x_{3mag})$')
+ax6.set_title('Flattened $(x_{3real})$')
+ax7.set_title('Flattened $(x_{3mag})$')
 
-# Set up bounding boxes
+# bounding boxes
 rect1 = patches.Rectangle(xy=(n1, n2), width=w, height=h, fill=False, color='blue', ls='--')
 rect2 = patches.Rectangle(xy=(m1, m2), width=w, height=h, fill=False, color='red')
 
-# Initial plots
+# scene image & bounding boxes plots
 img_scene = ax0.imshow(scene.real, extent=[0, N1, 0, N2], origin='lower', cmap='gray', interpolation=None, vmin=scene.min, vmax=scene.max)
 box1 = ax0.add_patch(rect1)
 box2 = ax0.add_patch(rect2)
-img1 = ax1.imshow(x3.mag, extent=[-w/2, w/2, -h/2, h/2], origin='lower', cmap='Reds', interpolation=None, vmin=x3.min, vmax=x3.max)
+
+# motion estimation image plot
+img1 = ax1.imshow(x3.real, extent=[-w/2, w/2, -h/2, h/2], origin='lower', cmap='Reds', interpolation=None, vmin=x3.min, vmax=x3.max)
+
+# foci image plots
 img2 = ax2.imshow(x1.real, extent=[0, h, 0, w], origin='lower', cmap='gray', interpolation=None, vmin=x1.min, vmax=x1.max)
 img3 = ax3.imshow(x2.real, extent=[0, h, 0, w], origin='lower', cmap='gray', interpolation=None, vmin=x2.min, vmax=x2.max)
 
+# motion estimation stats plots
 lines = []
-for ydata, ax in zip(ydatas.T, axes_stats):
+for ydata, ax in zip(ydatas.data.T, axes_stats):
     lines.append(ax.plot(xdata[:i], ydata[:i]))
-line1, = ax6.plot(x3.mag.flatten())
-point1, = ax6.plot(peak, x3.max, 'ro')
 
+# motion estimation line plot
+line1, = ax6.plot(x3.real.flatten())
+point1, = ax6.plot(peakreal, x3.max, 'ro')
+ax6.axhline(0, color='black', lw=1)
+line2, = ax7.plot(x3.mag.flatten())
+point2, = ax7.plot(peakmag, x3.max, 'ro')
+ax7.axhline(0, color='black', lw=1)
 
 def update(x, y):
-    global i
-    i += 1
-
-    # calculate new coords
-    m1, m2 = coords_bottomleft((x, y), (N1, N2), (w, h))
+    # calculate new bottom-left coords from new center coords
+    m1, m2 = coords_bottomleft((x, y), (N2, N1), (h, w))
 
     # create new windowed image with new coords
     x2 = scene.arr[m2: m2+h, m1: m1+w]
     x2 = np.multiply(window, x2)
-    x2 = signal(x2)
+    x2 = SignalCoords(x2, (m1, m2))
 
     # image processing
     X2 = fft2(x2.arr)
-    x3 = phase_corr2d(X1, X2)
-    x3 = signal(x3)
-
-    # update data tracking
-    ydatas[i%n_points, :] = update_ydatas(x3)
+    x3 = phase_correlation2d(X1, X2) # returns the normalized cross-correlation between X1 and X2
+    x3 = fftshift(x3)
+    x3 = Signal(x3)
 
     # calc shifts
-    a1, a2 = m1-n1, m2-n2 # actual shift
-    e2, e1 = np.where(x3.mag == np.max(x3.mag)) # estimated shift
-    e1, e2 = e1[0] - int(w/2), e2[0] - int(h/2) # adjusted for center origin plot
-    peak = np.where(x3.mag.flatten() == x3.max)[0]
+    dx = x2.x - x1.x # actual shift
+    dy = x2.y - x1.y
+    edy, edx = [int(x) for x in np.where(x3.real == x3.max)] # estimated shift
+    edx -= int(w/2) # adjust for fftshift of x3
+    edy -= int(h/2)
+    edx = unroll_shift(edx, w)
+    edy = unroll_shift(edy, h)
+    peakreal = np.where(x3.real.flatten() == x3.max)[0][0]
+    peakmag = np.where(x3.mag.flatten() == np.max(x3.mag))[0][0]
 
-    # update plots
+    # update data tracking
+    ydatas.update(new_ydata(x3))
+    i = ydatas.i
+
+    # update box, image, stats plots
     box2.set_xy((m1, m2))
-    img1.set_data(x3.mag)
+    img1.set_data(x3.real)
     img3.set_data(x2.real)
-    for ydata, line, ax in zip(ydatas.T, lines, axes_stats):
-        line[0].set_data(xdata[:i%n_points], ydata[:i%n_points])
-    line1.set_ydata(x3.mag.flatten())
-    point1.set_data(peak, x3.max)
-    ax0.set_title(f'Scene\nActual Shift = {a1, a2}')
-    ax1.set_title(f'Motion Estimate\nEstimated Shift = {e1, e2}')
-    ax6.set_title('$Flattened x_{3mag}$ \n'f'Peak at: {peak}')
+    for ydata, line, ax in zip(ydatas.data.T, lines, axes_stats):
+        line[0].set_data(xdata[:i], ydata[:i])
+    line1.set_ydata(x3.real.flatten())
+    point1.set_data(peakreal, x3.max)
+    line2.set_ydata(x3.mag.flatten())
+    point2.set_data(peakmag, np.max(x3.mag))
+
+    # update titles
+    ax0.set_title(f'Scene\nActual Shift = {dx, dy}')
+    ax1.set_title(f'Motion Estimate\nEstimated Shift = {edx, edy}')
+    ax6.set_title('$Flattened x_{3real}$')
+    ax7.set_title('$Flattened x_{3mag}$')
 
     fig1.canvas.draw_idle()
 
@@ -183,9 +238,8 @@ def mouse_move(event, LMB):
     if (LMB.isdown and ax == ax0):
         update(x, y)
 
-
+# event trackers
 plt.connect('motion_notify_event', lambda event : mouse_move(event, LMB))
 plt.connect('button_press_event', lambda event : mouse_click(event, LMB))
 plt.connect('button_release_event', lambda event : mouse_release(event, LMB))
 plt.show(block=True)
-
