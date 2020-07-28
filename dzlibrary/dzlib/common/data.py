7333c1a8a70aca672f99633f5f48109d935a8ca4
dziyaee <dziyaee@gmail.com>
2020-07-27 22:14:00 -0700

    created new module that will contain custom classes or subclasses to hold commonly used data. added Shape class and Array class (a subclass of ndarray)

--- dzlibrary/dzlib/common/data.py
+++ dzlibrary/dzlib/common/data.py
@@ -1,5 +1,28 @@
 
 
+class Array(np.ndarray):
+    '''Numpy Subclass to add predefined dimension attributes that update dynamically'''
+    attrs = ['num', 'depth', 'height', 'width'] # desired dimension attributes
+
+    def __new__(cls, input_array, ndmin=0):
+        obj = np.array(input_array, ndmin=ndmin).view(cls)
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None: return
+
+        ndim = self.ndim
+        attrs = self.attrs
+        shape = self.shape
+
+        ndim = min(ndim, len(attrs)) # get minimum between current shape and number of dimension attributes
+        attrs = attrs[-ndim:]
+        shape = shape[-ndim:]
+
+        for attr, dim in zip(attrs, shape):
+            setattr(self, attr, dim)
+
+
 class Shape:
     def __init__(self, shape):
         self.shape = shape
