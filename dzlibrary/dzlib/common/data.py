89f8bdf2ad8c3f06e8d1a2749c9c17b8bef3ea16
dziyaee <dziyaee@gmail.com>
2020-07-30 23:06:55 -0700

    added some validation comments

--- dzlibrary/dzlib/common/data.py
+++ dzlibrary/dzlib/common/data.py
@@ -35,16 +35,20 @@ class Shape:
     @shape.setter
     def shape(self, shape_):
         # validations
+        # type must be tuple or list
         if not isinstance(shape_, (tuple, list)):
             raise TypeError(f"Expected tuple or list, got {type(shape_)}")
 
+        # len must be > 0
         if len(shape_) == 0:
             raise ValueError(f"Expected a tuple or list of length > 0, got {len(shape_)}")
 
         for x in shape_:
+            # type must be int
             if not isinstance(x, int):
                 raise TypeError(f"elements in {shape_} must be of type (int), got {type(x)}")
 
+            # value must be > 0
             if not x > 0:
                 raise ValueError(f"elements in {shape_} must be > 0, got {x}")
 
@@ -64,54 +68,3 @@ class Shape:
     height = property(lambda self: self._get_dim(-2))
     depth = property(lambda self: self._get_dim(-3))
     num = property(lambda self: self._get_dim(-4))
-
-
-# # Alternate implementation of Shape Class using Dimension Class as Shape Class attributes
-# class Dimension:
-#     def __init__(self, n):
-#         self.n = n
-
-#     def __get__(self, obj, cls):
-#         if obj is None:
-#             return self
-#         try:
-#             return obj.shape[self.n]
-#         except IndexError:
-#             return None
-
-#     def __set__(self, obj, value):
-#         raise AttributeError("can't set attribute")
-
-
-# class Shape:
-#     width = Dimension(-1)
-#     height = Dimension(-2)
-#     depth = Dimension(-3)
-#     num = Dimension(-4)
-
-#     def __init__(self, shape):
-#         self.shape = shape
-
-#     @property
-#     def shape(self):
-#         return self._shape
-
-#     @shape.setter
-#     def shape(self, shape_):
-#         if not isinstance(shape_, (tuple, list)):
-#             raise TypeError(f"Expected tuple or list, got {type(shape_)}")
-
-#         if len(shape_) == 0:
-#             raise ValueError(f"Expected a tuple or list of length > 0, got {len(shape_)}")
-
-#         if  not all(isinstance(dim, int) for dim in shape_):
-#             raise TypeError(f"Expected only integer elements, got {shape_}")
-
-#         if not all(dim > 0 for dim in shape_):
-#             raise ValueError(f"Expected only positive integer elements, got {shape_}")
-
-#         self._shape = tuple(shape_)
-
-#     @property
-#     def ndim(self):
-#         return len(self.shape)
