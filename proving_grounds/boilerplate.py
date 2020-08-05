bbea6dde1c467073d4ced694a12a053c3a83fcf7
dziyaee <dziyaee@gmail.com>
2020-08-05 13:35:50 -0700

    removed ShapeNd class. refactored and moved to signal_processing package

---
+++ proving_grounds/boilerplate.py
@@ -0,0 +1,6 @@
+
+## programmatic class properties example
+# width =  property(lambda self: self._get_dim(-1), lambda self, val: self._set_dim(-1, val))
+# height = property(lambda self: self._get_dim(-2), lambda self, val: self._set_dim(-2, val))
+# depth =  property(lambda self: self._get_dim(-3), lambda self, val: self._set_dim(-3, val))
+# num =    property(lambda self: self._get_dim(-4), lambda self, val: self._set_dim(-4, val))
