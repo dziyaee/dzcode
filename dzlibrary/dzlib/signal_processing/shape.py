3756b80861643f1c884a98d426ad531f0a937729
dziyaee <dziyaee@gmail.com>
2020-08-06 00:05:54 -0700

    added an example to main

--- dzlibrary/dzlib/signal_processing/shape.py
+++ dzlibrary/dzlib/signal_processing/shape.py
@@ -43,3 +43,40 @@ class Dimension():
 
     def __set__(self, obj, val):
         obj._shape[self.n] = val
+
+
+if __name__ == "__main__":
+    # Example of subclassing ShapeNd and using the Dimension "helper" class as it's used in conjunction with Sweep2d class
+    # from shape import ShapeNd, Dimension
+
+    class Input4d(ShapeNd):
+        def __init__(self, shape):
+            super(Input4d, self).__init__(shape)
+
+        width = Dimension(-1)
+        height = Dimension(-2)
+        depth = Dimension(-3)
+        num = Dimension(-4)
+
+    class Param2d(ShapeNd):
+        def __init__(self, shape):
+            super(Param2d, self).__init__(shape)
+
+        width = Dimension(-1)
+        height = Dimension(-2)
+
+
+    images_shape = (1, 3, 10, 10)
+    kernels_shape = (1, 3, 3, 3)
+    padding = (2, 2)
+    stride = (1, 1)
+
+    images_shape = Input4d(images_shape)
+    kernels_shape = Input4d(kernels_shape)
+    padding = Param2d(padding)
+    stride = Param2d(stride)
+
+    print(images_shape)
+    print(kernels_shape)
+    print(padding)
+    print(stride)
