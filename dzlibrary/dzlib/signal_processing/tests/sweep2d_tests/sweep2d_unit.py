2bc304034965aee423b86e62a1cf04267618229b
dziyaee <dziyaee@gmail.com>
2020-08-15 19:41:22 -0700

    rewrote param generator for better readability, updated settings yaml

--- dzlibrary/dzlib/signal_processing/tests/sweep2d_tests/sweep2d_unit.py
+++ dzlibrary/dzlib/signal_processing/tests/sweep2d_tests/sweep2d_unit.py
@@ -4,10 +4,11 @@ def generate_shape_test_params(settings):
     '''Function to parse and return Sweep2d input args from settings dict loaded from a settings yaml'''
     settings = settings['Sweep2d']['Unit']
 
-    modes = settings['modes']
-    tests = settings['tests']
+    # input arg shapes
+    tests = list(settings['tests'].values())  # returns list of dicts of input arg lists per test
+    inputs = [list(test.values()) for test in tests]  #  converts input args dicts to lists
+    inputs = [[tuple(arg) for arg in args] for args in inputs]  # converts input arg lists to tuples
 
-    # this can be done in a one-liner, but it's less readable that way (imo)
-    inputs = [test.values() for test in tests.values()]  # returns list of dict_values of list of input args
-    inputs = [[tuple(input_arg) for input_arg in input_args] for input_args in inputs]  # returns list of list of tuples of input args
+    # input arg modes
+    modes = settings['modes']
     return inputs, modes
