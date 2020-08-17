9cdb9e1f0060eec1791f8ad857c06d7beb155530
dziyaee <dziyaee@gmail.com>
2020-08-16 22:38:22 -0700

    fixed bug in expected2 fixture. output[1] was = unpadded_depth, should have been = window_num

--- dzlibrary/dzlib/signal_processing/tests/sweep2d_tests/test_sweep2d_shape_values.py
+++ dzlibrary/dzlib/signal_processing/tests/sweep2d_tests/test_sweep2d_shape_values.py
@@ -9,8 +9,6 @@ settings_path = 'settings.yml'
 with open(settings_path) as file:
     settings = yaml.load(file, Loader=yaml.FullLoader)
 shapes_list, mode_list = generate_shape_test_params(settings)
-shapes_list = shapes_list[:1]
-
 
 # Test Fixtures
 scope = "module"
@@ -26,10 +24,6 @@ def mode(request):
 def shapes(request):
     shapes = request.param
     return shapes
-
-
-# @pytest.fixture(scope=scope)
-# def unpadded(request, )
 
 
 @pytest.fixture(scope=scope)
@@ -70,7 +64,7 @@ def expected2(expected1):
 
     output_height = ((padded_height - window_height) // stride_height) + 1
     output_width = ((padded_width - window_width) // stride_width) + 1
-    output = (unpadded_num, unpadded_depth, output_height, output_width)
+    output = (unpadded_num, window_num, output_height, output_width)
 
     im2col_height = window_depth * window_height * window_width
     im2col_width = output_height * output_width
@@ -85,95 +79,35 @@ def sweeper(shapes, mode):
 
 
 # Tests
-def test_unpadded(sweeper, expected1):
-    unpadded, *_ = expected1
-    assert sweeper.unpadded.shape == unpadded
+class Test_Shape_Values:
+    def test_unpadded(self, sweeper, expected1):
+        unpadded, *_ = expected1
+        assert sweeper.unpadded.shape == unpadded
 
+    def test_window(self, sweeper, expected1):
+        _, window, *_ = expected1
+        assert sweeper.window.shape == window
 
-def test_window(sweeper, expected1):
-    _, window, *_ = expected1
-    assert sweeper.window.shape == window
+    def test_padding(self, sweeper, expected1):
+        *_, padding, _ = expected1
+        assert sweeper.padding.shape == padding
 
+    def test_stride(self, sweeper, expected1):
+        *_, stride = expected1
+        assert sweeper.stride.shape == stride
 
-def test_padding(sweeper, expected1):
-    *_, padding, _ = expected1
-    assert sweeper.padding.shape == padding
+    def test_padded(self, sweeper, expected2):
+        padded, *_ = expected2
+        assert sweeper.padded.shape == padded
 
+    def test_padded_array(self, sweeper, expected2):
+        padded, *_ = expected2
+        assert sweeper.padded_array.shape == padded
 
-def test_stride(sweeper, expected1):
-    *_, stride = expected1
-    assert sweeper.stride.shape == stride
+    def test_output(self, sweeper, expected2):
+        _, output, _ = expected2
+        assert sweeper.output.shape == output
 
-
-def test_padded(sweeper, expected2):
-    padded, *_ = expected2
-    assert sweeper.padded.shape == padded
-
-
-def test_padded_array(sweeper, expected2):
-    padded, *_ = expected2
-    assert sweeper.padded_array.shape == padded
-
-
-def test_output(sweeper, expected2):
-    _, output, _ = expected2
-    assert sweeper.output.shape == output
-
-
-def test_im2col_indices(sweeper, expected2):
-    *_, im2col_indices = expected2
-    assert sweeper.im2col_indices.shape == im2col_indices
-
-
-
-
-# def test_padded_num(sweeper, expected):
-#     unpadded, *_ = expected
-#     assert sweeper.padded.shape[0] == unpadded[0]
-
-
-# def test_padded_depth(sweeper, expected):
-#     unpadded, *_ = expected
-#     assert sweeper.padded.shape[1] == unpadded[1]
-
-
-# def test_padded_height(sweeper, expected):
-#     unpadded, _, padding, _ = expected
-#     assert sweeper.padded.shape[2] == unpadded[2] + 2 * padding[0]
-
-
-# def test_padded_width(sweeper, expected):
-#     unpadded, _, padding, _ = expected
-#     assert sweeper.padded.shape[3] == unpadded[3] + 2 * padding[1]
-
-
-# def test_output_num(sweeper, expected):
-#     unpadded, *_ = expected
-#     assert sweeper.output.shape[0] == unpadded[0]
-
-
-# def test_output_depth(sweeper, expected):
-#     _, window, *_ = expected
-#     assert sweeper.output.shape[1] == window[0]
-
-
-# def test_output_height(sweeper, expected):
-#     unpadded, window, padding, stride = expected
-#     assert sweeper.output.shape[-2] == ((unpadded[-2] + 2 * padding[-2] - window[-2]) // stride[-2]) + 1
-
-
-# def test_output_width(sweeper, expected):
-#     unpadded, window, padding, stride = expected
-#     assert sweeper.output.shape[-1] == ((unpadded[-1] + 2 * padding[-1] - window[-1]) // stride[-1]) + 1
-
-
-# def test_im2col_height(sweeper, expected):
-#     _, window, *_ = expected
-#     assert sweeper.im2col_indices.shape[0] == window[1] * window[2] * window[3]
-
-
-# def test_im2col_width(sweeper, expected):
-#     unpadded, window, padding, stride = expected
-#     output_height = ((unpadded[-2] + 2 * padding[-2] - window[-2]) // stride[-2]) + 1
-#     output_width = ((unpadded[-1] + 2 * padding[-1] - window[-1]) // stride[-1]) + 1
-#     assert sweeper.im2col_indices.shape[1] == output_height * output_width
+    def test_im2col_indices(self, sweeper, expected2):
+        *_, im2col_indices = expected2
+        assert sweeper.im2col_indices.shape == im2col_indices
