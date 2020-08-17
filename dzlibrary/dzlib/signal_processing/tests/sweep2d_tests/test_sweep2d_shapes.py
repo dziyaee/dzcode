eb4a80303d77fa72d761ed682c2c095bf6de8f1b
dziyaee <dziyaee@gmail.com>
2020-08-17 13:37:10 -0700

    consolidated fixtures into conftest.py file and updated test modules accordingly

--- dzlibrary/dzlib/signal_processing/tests/sweep2d_tests/test_sweep2d_shapes.py
+++ dzlibrary/dzlib/signal_processing/tests/sweep2d_tests/test_sweep2d_shapes.py
@@ -1,40 +1,14 @@
 import numbers
-import yaml
 import pytest
-from dzlib.signal_processing.sweep2d import Sweep2d
-from dzlib.signal_processing.tests.sweep2d_tests.utils import generate_shape_test_params
 
 
 # All of the tests in this module are for data computed during Sweep2d class instatiation / initialization.
 
-# # Test Inputs
-settings_path = 'settings.yml'
-with open(settings_path) as file:
-    settings = yaml.load(file, Loader=yaml.FullLoader)
-inputs, modes = generate_shape_test_params(settings)
-
-# Test Fixtures
-scope = "module"  # scope must be shared by both fixtures as sweeper is dependent on mode
-
-
-@pytest.fixture(scope=scope, params=modes)
-def mode(request):
-    '''Fixture to iterate through each mode'''
-    return request.param
-
-
-@pytest.fixture(scope=scope, params=inputs)
-def sweeper(request, mode):
-    '''Fixture to iterate through each set of inputs, including modes from mode fixture'''
-    args = request.param
-    return Sweep2d(*args, mode)
-
-
 # Test Classes
-class Test_Shape_Lengths:
-    """
-    Length = 2: Padding, Stride, im2col indices array
-    Length = 4: Unpadded, Window, Output, Padded, Padded Array
+class Test_ShapeNd_Lengths:
+    """ Tests all object instances of ShapeNd for correct lengths of their .shape attributes (indirectly testing the ndim).
+    Length = 2: Padding, Stride
+    Length = 4: Unpadded, Window, Output, Padded
     """
 
     def test_padding_length(self, sweeper):
@@ -42,9 +16,6 @@ class Test_Shape_Lengths:
 
     def test_stride_length(self, sweeper):
         assert len(sweeper.stride.shape) == 2
-
-    def test_im2col_indices_length(self, sweeper):
-        assert len(sweeper.im2col_indices.shape) == 2
 
     def test_unpadded_length(self, sweeper):
         assert len(sweeper.unpadded.shape) == 4
@@ -58,12 +29,22 @@ class Test_Shape_Lengths:
     def test_padded_length(self, sweeper):
         assert len(sweeper.padded.shape) == 4
 
+
+class Test_Numpy_NdArray_Shape_Lengths:
+    """ Tests all object instances of numpy.ndarray for correct lengths of their .shape attributes (indirectly testing the ndim).
+    Length = 2: Padding, Stride
+    Length = 4: Unpadded, Window, Output, Padded
+    """
+
     def test_padded_array_length(self, sweeper):
         assert len(sweeper.padded_array.shape) == 4
 
+    def test_im2col_indices_length(self, sweeper):
+        assert len(sweeper.im2col_indices.shape) == 2
 
-class Test_Shape_Types:
-    '''
+
+class Test_ShapeNd_Types:
+    ''' Tests all object instances of ShapeNd objects for correct .shape type
     Shape is instance of tuple: unpadded, window, padding, stride, output, padded
     '''
 
@@ -86,58 +67,84 @@ class Test_Shape_Types:
         assert isinstance(sweeper.padded.shape, tuple)
 
 
-class Test_Shape_Dim_Types:
-    """
+class Test_ShapeNd_Dim_Types:
+    """ Tests all object instances of ShapeNd objects .shape attribute elements for correct type
     Element is instance of numbers.Integral: unpadded, window, padding (if mode is "user" or "full"), stride
     Element is instance of numbers.Real: padding (if mode is "same")
     """
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_unpadded_dim_type(self, sweeper_shape_dims):
+        unpadded_dim, *_ = sweeper_shape_dims
+        assert isinstance(unpadded_dim, numbers.Integral)
 
-    def test_unpadded_dim_type(self, sweeper):
-        for dim in sweeper.unpadded.shape:
-            assert isinstance(dim, numbers.Integral)
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_window_dim_type(self, sweeper_shape_dims):
+        _, window_dim, *_ = sweeper_shape_dims
+        assert isinstance(window_dim, numbers.Integral)
 
-    def test_window_dim_type(self, sweeper):
-        for dim in sweeper.window.shape:
-            assert isinstance(dim, numbers.Integral)
-
-    def test_padding_dim_type(self, sweeper):
-        if sweeper.mode == "same":
-            for dim in sweeper.padding.shape:
-                assert isinstance(dim, numbers.Real)
+    @pytest.mark.parametrize("sweeper_shape_dims", ["height", "width"], indirect=True)
+    def test_padding_dim_type(self, sweeper_shape_dims, mode):
+        _, _, padding_dim, *_ = sweeper_shape_dims
+        if mode == "same":
+            assert isinstance(padding_dim, numbers.Real)
 
         else:
-            for dim in sweeper.padding.shape:
-                assert isinstance(dim, numbers.Integral)
+            assert isinstance(padding_dim, numbers.Integral)
 
-    def test_stride_dim_type(self, sweeper):
-        for dim in sweeper.stride.shape:
-            assert isinstance(dim, numbers.Integral)
+    @pytest.mark.parametrize("sweeper_shape_dims", ["height", "width"], indirect=True)
+    def test_stride_dim_type(self, sweeper_shape_dims):
+        *_, stride_dim, _, _ = sweeper_shape_dims
+        assert isinstance(stride_dim, numbers.Integral)
+
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_padded_dim_type(self, sweeper_shape_dims):
+        *_, padded_dim, _ = sweeper_shape_dims
+        assert isinstance(padded_dim, numbers.Integral)
+
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_output_dim_type(self, sweeper_shape_dims):
+        *_, output_dim = sweeper_shape_dims
+        assert isinstance(output_dim, numbers.Integral)
 
 
 class Test_Shape_Dim_Min_Values:
-    '''
+    """ Tests all object instances of ShapeNd objects .shape attribute elements for greater than or equal to min value
+
     Element >= 1: unpadded, window, stride (if mode is "user")
     Element = 1: stride (if mode is "full" or "same")
     Element >= 0: padding
-    '''
+    """
 
-    def test_unpadded_dim_min_value(self, sweeper):
-        for dim in sweeper.unpadded.shape:
-            assert dim >= 1
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_unpadded_dim_min_value(self, sweeper_shape_dims):
+        unpadded_dim, *_ = sweeper_shape_dims
+        assert unpadded_dim >= 1
 
-    def test_window_dim_min_value(self, sweeper):
-        for dim in sweeper.window.shape:
-            assert dim >= 1
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_window_dim_min_value(self, sweeper_shape_dims):
+        _, window_dim, *_ = sweeper_shape_dims
+        assert window_dim >= 1
 
-    def test_padding_dim_min_value(self, sweeper):
-        for dim in sweeper.padding.shape:
-            assert dim >= 0
+    @pytest.mark.parametrize("sweeper_shape_dims", ["height", "width"], indirect=True)
+    def test_padding_dim_min_value(self, sweeper_shape_dims):
+        _, _, padding_dim, *_ = sweeper_shape_dims
+        assert padding_dim >= 0
 
-    def test_stride_dim_min_value(self, sweeper):
-        if sweeper.mode == "user":
-            for dim in sweeper.stride.shape:
-                assert dim >= 1
+    @pytest.mark.parametrize("sweeper_shape_dims", ["height", "width"], indirect=True)
+    def test_stride_dim_min_value(self, sweeper_shape_dims, mode):
+        *_, stride_dim, _, _ = sweeper_shape_dims
+        if mode == "user":
+            assert stride_dim >= 1
 
         else:
-            for dim in sweeper.stride.shape:
-                assert dim == 1
+            assert stride_dim == 1
+
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_padded_dim_min_value(self, sweeper_shape_dims):
+        *_, padded_dim, _ = sweeper_shape_dims
+        assert padded_dim >= 1
+
+    @pytest.mark.parametrize("sweeper_shape_dims", ["num", "depth", "height", "width"], indirect=True)
+    def test_output_dim_min_value(self, sweeper_shape_dims):
+        *_, output_dim = sweeper_shape_dims
+        assert output_dim >= 1
